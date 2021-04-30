379b5480e049265a4bd8828afac8984cc29dc8d1
cmontella <cmontella@live.com>
2021-04-29 23:05:38 -0400

    add #file/read

--- src/read.rs
+++ src/read.rs
@@ -5,9 +5,12 @@ use mech_utilities::{Machine, MachineRegistrar, RunLoopMessage};
 use std::thread::{self};
 use crossbeam_channel::Sender;
 use std::collections::HashMap;
+use std::fs;
 
 lazy_static! {
   static ref FILE_READ: u64 = hash_string("file/read");
+  static ref PATH: u64 = hash_string("path");
+  static ref CONTENTS: u64 = hash_string("contents");
 }
 
 export_machine!(file_read, file_read_reg);
@@ -33,7 +36,24 @@ impl Machine for Read {
   }
 
   fn on_change(&mut self, table: &Table) -> Result<(), String> {
-
+    for i in 1..=table.rows {
+      let filename = table.get_string(&TableIndex::Index(i), &TableIndex::Alias(*PATH));
+      match filename {
+        Some(filename) => {
+          let path = filename.clone();
+          let outgoing = self.outgoing.clone();
+          let row = TableIndex::Index(i);
+          let reader_handle = thread::spawn(move || {
+            let contents = fs::read_to_string(path).expect("Error reading file"); // TODO Send error
+            outgoing.send(RunLoopMessage::Transaction(Transaction{changes: vec![
+              Change::Set{table_id: *FILE_READ, values: vec![(row, TableIndex::Alias(*CONTENTS), Value::from_string(&contents))]},
+              Change::InternString{string: contents},
+            ]}));
+          });
+        }
+        None => (), // TODO Send error
+      }
+    }
     Ok(())
   }
 }
